ea10abbd74e050c5ebc7690a8be5f2588c6478d0
chasnew <chanuwas.asw@gmail.com>
2021-12-10 21:12:39 -0800

    create nested loops for feature extraction

--- prep_img_features.R
+++ prep_img_features.R
@@ -48,16 +48,11 @@ model.pool1 <- keras_model(inputs = base_model$input,
 model.fc2 <- keras_model(inputs = base_model$input,
                          outputs = get_layer(base_model, 'fc2')$output)
 
-batch_size <- 30
+batch_size <- 50
 
-# retrieve file names
+# retrieve file names (may need to edit to subset years)
 year_list <- reduced_art %>% 
   pull(yearStart) %>% unique()
-
-sample_paintings <- paintings %>% 
-  filter(yearStart >= 1900, yearStart <= 1970) %>% 
-  group_by(yearStart) %>% 
-  do(head(., batch_size))
 
 # reconstruct filename from OPP
 reconstruct_fn <- function(opp_str) {
@@ -67,65 +62,79 @@ reconstruct_fn <- function(opp_str) {
 img_dir <- "~/Box/QuantifyingPicasso/data_from_OPP/OPP_images"
 feature_dir <- "~/Box/QuantifyingPicasso/data_from_OPP/image_features"
 
-# enable local access to Box folders
 for (year in year_list) {
+  # enable local access to Box folder
   tmp_filelist <- list.files(file.path(img_dir, year, "ythumbs"))
-}
-
-# split samples into multiple batches/groups
-img_n <- length(sample_images)
-n_batch <- floor(img_n/batch_size)
-group_labels <- rep(1:n_batch, each = batch_size)
-rem <- img_n %% batch_size
-
-if (rem != 0) {
-  group_labels <- c(group_labels, rep(n_batch+1, rem))
-  n_batch = n_batch + 1
-}
-
-path_batches <- split(full_paths, f = group_labels)
-
-# extract features from each batch of images
-low_features <- c()
-high_features <- c()
-
-for (path_batch in path_batches) {
-  # constrctu file paths
-  sample_opps <- sample_paintings %>% 
+  
+  print(paste("year:", year))
+  flush.console()
+  
+  # retrieve filepaths of the year
+  sample_opps <- reduced_art %>% 
     filter(yearStart == year) %>% 
     pull(opp)
   
   opp_paths <- sample_opps %>% 
     reconstruct_fn() %>% 
     file.path(img_dir, year, "ythumbs", .)
+
+  # split samples into multiple batches/groups
+  img_n <- length(opp_paths)
+  n_batch <- floor(img_n/batch_size)
+  group_labels <- rep(1:n_batch, each = batch_size)
+  rem <- img_n %% batch_size
   
-  x <- map(opp_paths,
-           ~image_load(.x, target_size = c(224,224), grayscale = FALSE) %>% 
-             image_to_array() %>% 
-             divide_by(255)
-  ) # result in list
-  x <- array(unlist(x), dim = c(224, 224, 3, length(x))) %>% # convert into an array
-    aperm(c(4,1,2,3))
+  if (rem != 0) {
+    group_labels <- c(group_labels, rep(n_batch+1, rem))
+    n_batch = n_batch + 1
+  }
   
-  feature.conv1 <- model.conv1 %>%
-    predict(x) %>%
-    rowMeans(dims = 2)
-  feature.pool1 <- model.pool1 %>% 
-    predict(x) %>% 
-    rowMeans(dims = 2)
-  feature.fc2 <- model.fc2 %>% 
-    predict(x)
+  path_batches <- split(opp_paths, f = group_labels)
   
-  low_features <- rbind(low_features, cbind(feature.conv1, feature.pool1))
-  high_features <- rbind(high_features, feature.fc2)
+  # extract features from each batch of images
+  low_features <- c()
+  high_features <- c()
+  
+  for (path_batch in path_batches) {
+    # constrctu file paths
+    sample_opps <- sample_paintings %>% 
+      filter(yearStart == year) %>% 
+      pull(opp)
+    
+    opp_paths <- sample_opps %>% 
+      reconstruct_fn() %>% 
+      file.path(img_dir, year, "ythumbs", .)
+    
+    x <- map(opp_paths,
+             ~image_load(.x, target_size = c(224,224), grayscale = FALSE) %>% 
+               image_to_array() %>% 
+               divide_by(255)
+    ) # result in list
+    x <- array(unlist(x), dim = c(224, 224, 3, length(x))) %>% # convert into an array
+      aperm(c(4,1,2,3))
+    
+    feature.conv1 <- model.conv1 %>%
+      predict(x) %>%
+      rowMeans(dims = 2)
+    feature.pool1 <- model.pool1 %>% 
+      predict(x) %>% 
+      rowMeans(dims = 2)
+    feature.fc2 <- model.fc2 %>% 
+      predict(x)
+    
+    low_features <- rbind(low_features, cbind(feature.conv1, feature.pool1))
+    high_features <- rbind(high_features, feature.fc2)
+  }
+  
+  # store features in Box folder as csv
+  colnames(low_features) <- paste0("low_f", 1:336)
+  colnames(high_features) <- paste0("high_f", 1:4096)
+  
+  low_features %>% 
+    data.table() %>% 
+    fwrite(file.path(feature_dir, paste0("low_feature_", category, ".csv")))
+  
+  high_features %>% 
+    data.table() %>% 
+    fwrite(file.path(feature_dir, paste0("high_feature_", category, ".csv")))
 }
-
-# store features in Box folder as csv
-colnames(low_features) <- paste0("low_f", 1:336)
-colnames(high_features) <- paste0("high_f", 1:4096)
-
-low_features <- low_features %>% 
-  data.table()
-
-high_features <- high_features %>% 
-  data.table()
