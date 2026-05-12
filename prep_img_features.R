6f843ca1f8cb4957ed83e35bb46e70f76995f51e
chasnew <chanuwas.asw@gmail.com>
2026-05-12 16:04:32 -0700

    Updated pipeline

--- prep_img_features.R
+++ prep_img_features.R
@@ -1,130 +1,63 @@
 library(tidyverse)
-library(data.table)
-library(lubridate)
-library(stringr)
 library(imager)
-library(tensorflow)
-library(keras)
-
-home_dir <- path.expand("~")
-picasso_path <- file.path(home_dir, "Library/CloudStorage/Box-Box/QuantifyingPicasso")
-target_cat <- "painting"
-feature_model <- "resnet"
-
-# artwork tabular data
-art_data <- read.delim("raw_data/artwork1.csv", colClasses = "character") # /t as separator
-
-# row whose columns are shifted
-tmp_ind <- art_data[art_data$opp == "OPP.36:312",] %>% rownames()
-art_data[tmp_ind, 2] <- paste(art_data[tmp_ind, 2], art_data[tmp_ind, 3])
-for (i in 3:ncol(art_data)-1) {
-  art_data[tmp_ind, i] <- art_data[tmp_ind, i+1]
-}
-
-# parse out date, month, and year variables from existing columns
-art_data <- art_data %>% 
-  separate(dateEnd, c("yearEnd","monthEnd","dayEnd"), remove = FALSE) %>% 
-  separate(dateStart, c("yearStart","monthStart","dayStart"), remove = FALSE) %>% 
-  mutate(yearEnd = as.numeric(yearEnd),
-         monthEnd = as.numeric(monthEnd),
-         dayEnd = as.numeric(dayEnd),
-         yearStart = as.numeric(yearStart),
-         monthStart = as.numeric(monthStart),
-         dayStart = as.numeric(dayStart))
-
-# remove weird data (7 entries)
-art_data <- art_data %>%
-  filter(category != "") %>% # empty rows
-  filter(yearEnd != 0) # 2 entries w/ missing end dates
-
-# select necessary columns
-reduced_art <- art_data %>% 
-  filter(category == target_cat) %>% 
-  select(opp, title, category, yearStart)
+# library(tensorflow)
+# library(keras)
+library(keras3)
 
 # model loading
-
-if (feature_model == "vgg") {
-  # vgg model
-  base_model <- application_vgg16(weights = 'imagenet')
-  # intermediate layers
-  model.conv1 <- keras_model(inputs = base_model$input,
-                             outputs = get_layer(base_model, 'block1_conv1')$output)
-  model.pool1 <- keras_model(inputs = base_model$input,
-                             outputs = get_layer(base_model, 'block1_pool')$output)
-  model.fc <- keras_model(inputs = base_model$input,
-                           outputs = get_layer(base_model, 'fc2')$output)
-} else if (feature_model == "resnet") {
-  # resnet model
-  base_model <- application_resnet50(weights = 'imagenet')
-  # (None, 112, 112, 64)
-  model.conv1 <- keras_model(inputs = base_model$input,
-                             outputs = get_layer(base_model, 'conv1_conv')$output)
-  # (None, 56, 56, 64)
-  model.pool1 <- keras_model(inputs = base_model$input,
-                             outputs = get_layer(base_model, 'pool1_pool')$output)
-  # (None, 2048)
-  model.fc <- keras_model(inputs = base_model$input,
-                               outputs = get_layer(base_model, 'avg_pool')$output)
-}
-
-print("Completed loading models")
-
-
-# within-year batch size
-batch_size <- 75
-
-# input year range (min = 1889, max = 1973)
-start_year <- 1941
-end_year <- 1973
-arg_year_range <- start_year:end_year
-
-# retrieve file names (may need to edit to subset years)
-year_list <- reduced_art %>% 
-  pull(yearStart) %>% unique()
-
-truc_yearlist <- arg_year_range[arg_year_range %in% year_list]
-
-# reconstruct filename from OPP
-reconstruct_fn <- function(opp_str) {
-  return(paste0("yopp", substr(opp_str, 5, 6), "-", substr(opp_str, 8, 10), ".jpg"))
-}
-
-img_dir <- file.path(picasso_path, "data_from_OPP", "OPP_images")
-feature_dir <- file.path(picasso_path, "data_from_OPP", "image_features")
-
-# track processed year and opp ids
-trackl_name <- paste0("track_list_", target_cat, "_", feature_model, ".Rdata")
-if (file.exists(trackl_name)) {
-  load(trackl_name)
-} else {
-  track_list <- list(year = c(), opp = c(), not_found = c())
-}
-
-for (year in truc_yearlist) {
-  # enable local access to Box folder
-  tmp_filelist <- list.files(file.path(img_dir, year, "ythumbs"))
-  
-  print(paste("year:", year))
-  flush.console()
-  
-  # retrieve filepaths of the year
-  sample_opps <- reduced_art %>% 
-    filter(yearStart == year) %>% 
-    pull(opp)
-  
-  opp_paths <- sample_opps %>% 
-    reconstruct_fn() %>% 
-    file.path(img_dir, year, "ythumbs", .)
-  
-  # check if all files exist
-  not_found <- opp_paths[!file.exists(opp_paths)]
-  if (length(not_found) > 0) {
-    opp_paths <- opp_paths[file.exists(opp_paths)]
+ml_loading <- function(feature_model) {
+  if (feature_model == "vgg") {
+    # vgg model
+    base_model <- application_vgg16(weights = 'imagenet')
+    base_model$trainable <- FALSE
+    
+    # intermediate layers
+    model.conv1 <- keras_model(inputs = base_model$input,
+                               outputs = get_layer(base_model, 'block1_conv1')$output)
+    model.pool1 <- keras_model(inputs = base_model$input,
+                               outputs = get_layer(base_model, 'block1_pool')$output)
+    model.fc <- keras_model(inputs = base_model$input,
+                            outputs = get_layer(base_model, 'fc2')$output)
+  } else if (feature_model == "resnet") {
+    # resnet model
+    base_model <- application_resnet50(weights = 'imagenet')
+    base_model$trainable <- FALSE
+    
+    # (None, 112, 112, 64)
+    model.conv1 <- keras_model(inputs = base_model$input,
+                               outputs = get_layer(base_model, 'conv1_conv')$output)
+    # (None, 56, 56, 64)
+    model.pool1 <- keras_model(inputs = base_model$input,
+                               outputs = get_layer(base_model, 'pool1_pool')$output)
+    # (None, 2048)
+    model.fc <- keras_model(inputs = base_model$input,
+                            outputs = get_layer(base_model, 'avg_pool')$output)
+  } else if (feature_model == "effnet") {
+    # EfficientNet model
+    base_model <- application_efficientnet_b0(weights = 'imagenet')
+    base_model$trainable <- FALSE
+    
+    # (None, 112, 112, 32)
+    model.conv1 <- keras_model(inputs = base_model$input,
+                               outputs = get_layer(base_model, 'stem_conv')$output)
+    # (None, 112, 112, 32)
+    model.pool1 <- keras_model(inputs = base_model$input,
+                               outputs = get_layer(base_model, 'block1a_activation')$output)
+    # (None, 1280)
+    model.fc <- keras_model(inputs = base_model$input,
+                            outputs = get_layer(base_model, 'avg_pool')$output)
   }
+  
+  
+  ml_layers <- list(model.conv1, model.pool1, model.fc, base_model)
+  return(ml_layers)
+}
 
-  # split samples into multiple batches/groups
-  img_n <- length(opp_paths)
+
+# split samples into multiple batches/groups
+path_batching <- function(img_paths, batch_size) {
+  
+  img_n <- length(img_paths)
   
   # check if numger of images is smaller than batch size
   if (img_n < batch_size) {
@@ -144,9 +77,18 @@ for (year in truc_yearlist) {
   print(paste("image number:", img_n))
   print(paste("batch number:", n_batch))
   
-  path_batches <- split(opp_paths, f = group_labels)
+  path_batches <- split(img_paths, f = group_labels)
+  return(path_batches)
+}
+
+
+# extract features from each batch of images
+extract_features <- function(path_batches, ml_layers) {
+  model.conv1 <- ml_layers[[1]]
+  model.pool1 <- ml_layers[[2]]
+  model.fc <- ml_layers[[3]]
+  base_model <- ml_layers[[4]]
   
-  # extract features from each batch of images
   low_features <- c()
   high_features <- c()
   
@@ -158,24 +100,39 @@ for (year in truc_yearlist) {
     flush.console()
     b_count <- b_count + 1
     
+    # tmp <- application_preprocess_inputs(base_model, x)
+    # dim1 <- dim(tmp)[1]
+    # tmp <- array(tmp, dim = c(dim1, 224, 224, 3))
+    # tmp[1,,,1]
+    # x[1,,,3] - tmp[1,,,1]
+    # x[1,1,,1]
+    # tmp[1,1,,1]
+    
     # load images
     x <- map(path_batch,
-             ~image_load(.x, target_size = c(224,224), grayscale = FALSE) %>% 
-               image_to_array() %>% 
-               divide_by(255)
+             ~image_load(.x, target_size = c(224,224), color_mode = "rgb") %>% 
+               image_to_array()
+               # divide_by(255)
     ) # result in list
-    
-    # y <- map(path_batch,
-    #          ~image_load(.x, target_size = c(224,224), grayscale = FALSE)
-    # ) # result in list
-    # tmp_img <- y[[53]] %>% image_to_array()
-    # as.cimg(tmp_img) %>% plot(axes=FALSE)
-    
     x <- array(unlist(x), dim = c(224, 224, 3, length(x))) %>% # convert into an array
       aperm(c(4,1,2,3))
     
+    x <- application_preprocess_inputs(base_model, x)
+    x <- array(unlist(x), dim = c(dim(x)[1], 224, 224, 3))
+    
+    # summary(base_model)
+    # test_layer <- keras_model(inputs = base_model$input,
+    #                           outputs = get_layer(base_model, 'conv5_block3_out')$output)
+    # test.feature <- test_layer |>
+    #   predict(x)
+    # test.feature %>% dim()
+    # test.feature
+    # mean(test.feature[1,,,5], na.rm=T)
+    # avg_tmp <- apply(test.feature, c(1,4), mean, na.rm=T)
+    # avg_tmp[1,]
+    
     # extract image embeddings
-    feature.conv1 <- model.conv1 %>%
+    feature.conv1 <- model.conv1 %>% 
       predict(x) %>%
       rowMeans(dims = 2)
     feature.pool1 <- model.pool1 %>% 
@@ -184,31 +141,14 @@ for (year in truc_yearlist) {
     feature.fc <- model.fc %>% 
       predict(x)
     
+    # feature.fc[1,]
+    
     low_features <- rbind(low_features, cbind(feature.conv1, feature.pool1))
     high_features <- rbind(high_features, feature.fc)
   }
   
-  # store features in Box folder as csv
   colnames(low_features) <- paste0("low_f", 1:dim(low_features)[2])
   colnames(high_features) <- paste0("high_f", 1:dim(high_features)[2])
   
-  low_features %>% 
-    data.table() %>% 
-    fwrite(file.path(feature_dir, paste0("low_feature_", target_cat, "_", feature_model, ".csv")), append = TRUE)
-  
-  high_features %>% 
-    data.table() %>% 
-    fwrite(file.path(feature_dir, paste0("high_feature_", target_cat, "_", feature_model, ".csv")), append = TRUE)
-  
-  # store current year
-  track_list[["year"]] <- c(track_list[["year"]], year)
-  
-  # store opp ids
-  track_list[["opp"]] <- c(track_list[["opp"]], sample_opps)
-  
-  # store not found files
-  track_list[["not_found"]] <- c(track_list[["not_found"]], not_found)
-  
-  # store processed year and opp ids
-  save(track_list, file = trackl_name)
+  return(list(low_features, high_features))
 }
